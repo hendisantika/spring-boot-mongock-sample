be89b9fb3ad0a74cfc4ec7ec771248d4275b4d1f
hendisantika <hendisantika@yahoo.co.id>
2025-01-19 06:14:39 +0700

    feta: Add Test Containers configs🫸🌀✏️📗🐧🐳⬆

--- src/test/java/id/my/hendisantika/mongock/SpringBootMongockSampleApplicationTests.java
+++ src/test/java/id/my/hendisantika/mongock/SpringBootMongockSampleApplicationTests.java
@@ -1,13 +1,28 @@
 package id.my.hendisantika.mongock;
 
-import org.junit.jupiter.api.Test;
+import id.my.hendisantika.mongock.customer.CustomerRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-@SpringBootTest
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@Testcontainers
+@SpringBootTest(
+        properties = {
+                "management.endpoint.health.show-details=always",
+                "spring.datasource.url=jdbc:tc:mongodb:8.0.4:///customerDB"
+        },
+        webEnvironment = RANDOM_PORT
+)
 class SpringBootMongockSampleApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+    @Autowired
+    private CustomerRepository customerRepository;
 
+    @BeforeEach
+    void deleteAll() {
+        customerRepository.deleteAll();
+    }
 }
