ca69b65583ac528935f10de28fe8c35aa5a452e0
hendisantika <hendisantika@yahoo.co.id>
2025-01-19 05:56:55 +0700

    Add @EnableMongock🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/mongock/SpringBootMongockSampleApplication.java
+++ src/main/java/id/my/hendisantika/mongock/SpringBootMongockSampleApplication.java
@@ -1,8 +1,10 @@
 package id.my.hendisantika.mongock;
 
+import io.mongock.runner.springboot.EnableMongock;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableMongock
 @SpringBootApplication
 public class SpringBootMongockSampleApplication {
 
